0de7cba48925796f3753681f6d178083cd732dcd
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-03-06 01:31:09 +0500

    Base setup

--- app/src/main/java/com/example/android/unscramble/MainActivity.kt
+++ app/src/main/java/com/example/android/unscramble/MainActivity.kt
@@ -26,6 +26,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // main_activity.xml
         setContentView(R.layout.main_activity)
     }
+
 }
