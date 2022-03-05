556f4660a187ac69c524e8bfddb85c4de9a34587
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-03-06 02:35:40 +0500

    Implementing GameViewModel

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -5,6 +5,13 @@ import androidx.lifecycle.ViewModel
 // GameFragment will access information about the game from the GameViewModel
 class GameViewModel : ViewModel() {
 
+    private var score = 0
+    private var currentWordCount = 0
 
+
+    // Backing Property
+    private var _currentScrambledWord = "test"
+    val currentScrambledWord: String
+        get() = _currentScrambledWord
 
 }
